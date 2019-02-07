3d5240e9fe44930df35bd4e128f3c627c0238edb
Tara Sherman <taradelin@gmail.com>
2019-02-06 20:51:24 -0500

    this.js code examples

--- assignments/this.js
+++ assignments/this.js
@@ -20,11 +20,40 @@ windowBinding();
 // Principle 2
 
 // code example for Implicit Binding
+const myInfo = function (name, shoeSize) {
+    return {
+        name: name,
+        shoeSize: shoeSize,
+        whatSize: function () {
+            console.log(this.shoeSize);
+        },
+        myFriend: {
+            shoeSize: 11,
+            whatSize: function () {
+                console.log(this.shoeSize);
+            }
+        }
+    };
+};
 
+tara = myInfo("Tara", 7);
+tara.whatSize(); 7
+tara.myFriend.whatSize(); 11;
 
 // Principle 3
 
 // code example for New Binding
+const lensRx = function (sphere, cyl, axis) {
+    this.sphere - sphere;
+    this.cyl = cyl;
+    this.axis = axis;
+};
+
+let ODRx = new lensRx('-6.50', '-0.25', 164);
+console.log(ODRx);
+
+let OSRx = new lensRx('-6.00', '-0.50', 122);
+console.log(OSRx);
 
 // Principle 4
 
